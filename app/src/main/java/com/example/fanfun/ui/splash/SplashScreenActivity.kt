effa6b80ae106adb4d9f5c912ea9c411c7fdaeac
Nicolas Alfaro <nalfaro@jumpitt.com>
2020-11-04 16:50:45 -0300

    splash added as theme

---
+++ app/src/main/java/com/example/fanfun/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.example.fanfun.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fanfun.ui.login.LoginActivity
+
+
+class SplashScreenActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this , LoginActivity::class.java))
+        finish()
+    }
+
+}
